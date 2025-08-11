0e728e0210e00164b0f8f1a8673c4167578e427a
subbotaMan <tabolskysergei@yandex.ru>
2025-08-11 12:19:54 +0300

    Add plain formatter

---
+++ bin/gendiff.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import { Command } from 'commander'
+import genDiff from '../src/index.js'
+
+const program = new Command()
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'display help for command')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    const result = genDiff(filepath1, filepath2, options.format)
+    console.log(result)
+  })
+
+program.parse(process.argv)
+
+if (process.argv.length === 2) {
+  program.help()
+}
