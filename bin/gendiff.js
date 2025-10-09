812c3f2fc91f9bb292d0d9eecc639570f0090ab5
subbotaMan <tabolskysergei@yandex.ru>
2025-10-09 12:47:22 +0300

    commander option change

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(gendiff(filepath1, filepath2, program.format))
   })
